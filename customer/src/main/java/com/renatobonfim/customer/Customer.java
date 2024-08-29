131d0fcbd395968e6f015ba743047bcb1c428e31
renatu <renatu_mb@yahoo.com.br>
2024-08-29 11:16:09 -0700

    Customer ( db integration )

--- customer/src/main/java/com/renatobonfim/customer/Customer.java
+++ customer/src/main/java/com/renatobonfim/customer/Customer.java
@@ -1,11 +1,23 @@
 package com.renatobonfim.customer;
 
+import javax.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Customer {
+
+    @Id
+    @SequenceGenerator(name = "customer_id_sequence")
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "customer_id_sequence")
+    private Long id;
+
     String firstName;
     String lastName;
     String email;
--- customer/src/main/java/com/renatobonfim/customer/CustomerService.java
+++ customer/src/main/java/com/renatobonfim/customer/CustomerService.java
@@ -1,14 +1,23 @@
 package com.renatobonfim.customer;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class CustomerService {
+
+    CustomerRepository customerRepository;
+
     public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
         Customer customer = Customer.builder()
                 .firstName(customerRegistrationRequest.firstName())
                 .lastName(customerRegistrationRequest.lastName())
-                .email(customerRegistrationRequest.lastName())
+                .email(customerRegistrationRequest.email())
                 .build();
+        // todo: check if email is valid
+        // todo: check if firstName is valid
+        // todo: check fi lastName is valid
+        customerRepository.save(customer);
     }
 }
